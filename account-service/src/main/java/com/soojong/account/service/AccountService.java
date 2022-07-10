113d60303a8b07575fc6b5f334e933ed3e7cf39e
92SooJong <hsz3855@gmail.com>
2022-07-10 22:25:09 +0900

    COUNT_BASED 서킷 브레이커 완성

--- account-service/src/main/java/com/soojong/account/service/AccountService.java
+++ account-service/src/main/java/com/soojong/account/service/AccountService.java
@@ -5,27 +5,30 @@ import com.soojong.account.entity.Account;
 import com.soojong.account.repository.AccountRepository;
 import lombok.RequiredArgsConstructor;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class AccountService {
 
-    private final AccountRepository accountRepository;
     private final CustomerCompositeService customerCompositeService;
 
-    public Long createAccount(Account account) throws Exception {
+    public Long createAccount(Account account){
 
 
-        Optional<Customer> optionalCustomer = customerCompositeService.retrieveCustomer(account.getCustomerId());
+        for(int i=0; i<20; i++){
+            log.info("{} 번째 요청",i+1);
+            Optional<Customer> optionalCustomer = customerCompositeService.retrieveCustomer(account.getCustomerId());
+        }
 
-        Customer customer = optionalCustomer.orElseThrow(() -> new Exception("고객정보가 없습니다!"));
+        //Customer customer = optionalCustomer.orElseThrow(() -> new Exception("고객정보가 없습니다!"));
+        //account.setCustomerName(customer.getCustomerName());
 
-        account.setCustomerName(customer.getCustomerName());
-
-        return accountRepository.save(account).getId();
+        return null;//accountRepository.save(account).getId();
     }
 
 }
--- account-service/src/main/java/com/soojong/account/service/CustomerCompositeService.java
+++ account-service/src/main/java/com/soojong/account/service/CustomerCompositeService.java
@@ -11,20 +11,20 @@ import org.springframework.web.client.RestTemplate;
 
 import java.util.Optional;
 
-@Service
 @Slf4j
+@Service
 @RequiredArgsConstructor
 public class CustomerCompositeService {
 
     @Value("${customer.api.url}")
     private String CUSTOMER_API_URL;
-
     private final RestTemplate restTemplate;
 
 
     @CircuitBreaker(name = "backendA",fallbackMethod = "fallbackRetrieveCustomer")
     public Optional<Customer> retrieveCustomer(Long customerId){
         String apiUrl = CUSTOMER_API_URL + "/api/v1/{customer-id}";
+
         Customer forObject = restTemplate.getForObject(apiUrl, Customer.class, customerId);
 
         return Optional.ofNullable(forObject);
@@ -32,11 +32,10 @@ public class CustomerCompositeService {
 
     public Optional<Customer> fallbackRetrieveCustomer(Long customerId,Exception ex){
 
-        System.out.println(customerId+"의 고객 정보를 불러오지 못했습니다");
+        log.info(ex.getClass()+" => "+ex.getMessage());
+        log.info("{} ID를 가진 고객 정보를 불러오지 못했습니다",customerId);
         return Optional.empty();
 
     }
-
-
 
 }
